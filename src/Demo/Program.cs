185a9c7b6220d3cdeedd4972ab74564c58674302
Pierre <pierre.darcas@hotmail.fr>
2021-10-18 14:46:08 +0200

    Logger + env parameter

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -3,8 +3,37 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using Fibonacci;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+
+var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    IConfiguration configuration = new ConfigurationBuilder()        
+        .SetBasePath(Directory.GetCurrentDirectory())        
+        .AddEnvironmentVariables()        
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)        
+        .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)        
+        .Build();    
+    var applicationSection = configuration.GetSection("Application");    
+    var applicationConfig = applicationSection.Get<ApplicationConfig>();
+    Console.WriteLine($"Application Name : {applicationConfig.Name}");
+    Console.WriteLine($"Application Message : {applicationConfig.Message}");
+    
+
+    var loggerFactory = LoggerFactory.Create(builder => {
+        builder.AddFilter("Microsoft", LogLevel.Warning)
+            .AddFilter("System", LogLevel.Warning)
+            .AddFilter("Demo", LogLevel.Debug)
+            .AddConsole();
+        
+    });
+    var logger = loggerFactory.CreateLogger("Demo.Program");
+    
+    logger.LogInformation($"Application Name : {applicationConfig.Name}");
+    logger.LogInformation($"Application Message : {applicationConfig.Message}");
 
 Console.WriteLine(Fibonacci.Class1.Test());
 
@@ -68,3 +97,8 @@ Console.WriteLine($"{stopwatch.Elapsed.Seconds} s");
     return list;
 }*/
 
+public class ApplicationConfig
+{
+    public string Name { get; set; }
+    public string Message { get; set; }
+}
